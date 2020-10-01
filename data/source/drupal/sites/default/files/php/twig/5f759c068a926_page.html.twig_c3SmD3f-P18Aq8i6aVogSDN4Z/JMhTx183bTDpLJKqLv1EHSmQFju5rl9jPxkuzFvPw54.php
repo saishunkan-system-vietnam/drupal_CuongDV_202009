8737a84059592271d32a236ba9c2ffef4a8b018c
Dk Cuong <54566571+dkcuongz@users.noreply.github.com>
2020-10-01 17:46:29 +0700

    Soure 8.9.6

---
+++ data/source/drupal/sites/default/files/php/twig/5f759c068a926_page.html.twig_c3SmD3f-P18Aq8i6aVogSDN4Z/JMhTx183bTDpLJKqLv1EHSmQFju5rl9jPxkuzFvPw54.php
@@ -0,0 +1,270 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/seminar/templates/layout/page.html.twig */
+class __TwigTemplate_d2e32ab7ac79fa8a392245e1726285ab4fc9e479302b71cbe9dcf8895c4167ea extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 52];
+        $filters = ["escape" => 49];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 46
+        echo "<div class=\"layout-container\">
+
+  <header role=\"banner\">
+    ";
+        // line 49
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "header", [])), "html", null, true);
+        echo "
+  </header>
+";
+        // line 51
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "sidebar_first", [])), "html", null, true);
+        echo "
+  ";
+        // line 52
+        if ($this->getAttribute(($context["page"] ?? null), "seminar_menu", [])) {
+            // line 53
+            echo "  <div id=\"mythememenu\" class=\"content\">
+    ";
+            // line 54
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "seminar_menu", [])), "html", null, true);
+            echo "
+  </div>
+  ";
+        }
+        // line 57
+        echo "  ";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "breadcrumb", [])), "html", null, true);
+        echo "
+
+  ";
+        // line 59
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "highlighted", [])), "html", null, true);
+        echo "
+
+  ";
+        // line 61
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "help", [])), "html", null, true);
+        echo "
+
+  <main role=\"main\">
+    <a id=\"main-content\" tabindex=\"-1\"></a>";
+        // line 65
+        echo "
+    <div class=\"layout-content\">
+      ";
+        // line 67
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "content", [])), "html", null, true);
+        echo "
+    </div>";
+        // line 69
+        echo "
+    ";
+        // line 70
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_first", [])) {
+            // line 71
+            echo "      <aside class=\"layout-sidebar-first\" role=\"complementary\">
+        ";
+            // line 72
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "sidebar_first", [])), "html", null, true);
+            echo "
+      </aside>
+    ";
+        }
+        // line 75
+        echo "
+    ";
+        // line 76
+        if ($this->getAttribute(($context["page"] ?? null), "sidebar_second", [])) {
+            // line 77
+            echo "      <aside class=\"layout-sidebar-second\" role=\"complementary\">
+        ";
+            // line 78
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "sidebar_second", [])), "html", null, true);
+            echo "
+      </aside>
+    ";
+        }
+        // line 81
+        echo "
+  </main>
+
+  ";
+        // line 84
+        if ($this->getAttribute(($context["page"] ?? null), "footer", [])) {
+            // line 85
+            echo "    <footer role=\"contentinfo\">
+      ";
+            // line 86
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "footer", [])), "html", null, true);
+            echo "
+    </footer>
+  ";
+        }
+        // line 89
+        echo "
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/seminar/templates/layout/page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  149 => 89,  143 => 86,  140 => 85,  138 => 84,  133 => 81,  127 => 78,  124 => 77,  122 => 76,  119 => 75,  113 => 72,  110 => 71,  108 => 70,  105 => 69,  101 => 67,  97 => 65,  91 => 61,  86 => 59,  80 => 57,  74 => 54,  71 => 53,  69 => 52,  65 => 51,  60 => 49,  55 => 46,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override to display a single page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.html.twig template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - base_path: The base URL path of the Drupal installation. Will usually be
+ *   \"/\" unless you have installed Drupal in a sub-directory.
+ * - is_front: A flag indicating if the current page is the front page.
+ * - logged_in: A flag indicating if the user is registered and signed in.
+ * - is_admin: A flag indicating if the user has permission to access
+ *   administration pages.
+ *
+ * Site identity:
+ * - front_page: The URL of the front page. Use this instead of base_path when
+ *   linking to the front page. This includes the language domain or prefix.
+ *
+ * Page content (in order of occurrence in the default page.html.twig):
+ * - messages: Status and error messages. Should be displayed prominently.
+ * - node: Fully loaded node, if there is an automatically-loaded node
+ *   associated with the page and the node ID is the second argument in the
+ *   page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - page.header: Items for the header region.
+ * - page.primary_menu: Items for the primary menu region.
+ * - page.secondary_menu: Items for the secondary menu region.
+ * - page.highlighted: Items for the highlighted content region.
+ * - page.help: Dynamic help text, mostly for admin pages.
+ * - page.content: The main content of the current page.
+ * - page.sidebar_first: Items for the first sidebar.
+ * - page.sidebar_second: Items for the second sidebar.
+ * - page.footer: Items for the footer region.
+ * - page.breadcrumb: Items for the breadcrumb region.
+ *
+ * @see template_preprocess_page()
+ * @see html.html.twig
+ */
+#}
+<div class=\"layout-container\">
+
+  <header role=\"banner\">
+    {{ page.header }}
+  </header>
+{{ page.sidebar_first }}
+  {% if page.seminar_menu %}
+  <div id=\"mythememenu\" class=\"content\">
+    {{ page.seminar_menu}}
+  </div>
+  {% endif %}
+  {{ page.breadcrumb }}
+
+  {{ page.highlighted }}
+
+  {{ page.help }}
+
+  <main role=\"main\">
+    <a id=\"main-content\" tabindex=\"-1\"></a>{# link is in html.html.twig #}
+
+    <div class=\"layout-content\">
+      {{ page.content }}
+    </div>{# /.layout-content #}
+
+    {% if page.sidebar_first %}
+      <aside class=\"layout-sidebar-first\" role=\"complementary\">
+        {{ page.sidebar_first }}
+      </aside>
+    {% endif %}
+
+    {% if page.sidebar_second %}
+      <aside class=\"layout-sidebar-second\" role=\"complementary\">
+        {{ page.sidebar_second }}
+      </aside>
+    {% endif %}
+
+  </main>
+
+  {% if page.footer %}
+    <footer role=\"contentinfo\">
+      {{ page.footer }}
+    </footer>
+  {% endif %}
+
+</div>{# /.layout-container #}
+", "themes/seminar/templates/layout/page.html.twig", "/var/www/html/drupal/themes/seminar/templates/layout/page.html.twig");
+    }
+}
