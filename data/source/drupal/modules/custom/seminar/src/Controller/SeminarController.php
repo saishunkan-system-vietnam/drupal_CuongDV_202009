10285340831ad289d66e7f1a5c352020267ab15a
Dk Cuong <54566571+dkcuongz@users.noreply.github.com>
2020-10-07 17:48:24 +0700

    update custom theme

---
+++ data/source/drupal/modules/custom/seminar/src/Controller/SeminarController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\seminar\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Database\Database;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+/**
+ * Provides route responses for the Example module.
+ */
+class SeminarController extends ControllerBase
+{
+    protected $request;
+    protected $formBuilder;
+    protected $session;
+    protected $user;
+    protected $conn;
+
+    public function __construct()
+    {
+        $this->request = \Drupal::request();
+        $this->formBuilder = \Drupal::formBuilder();
+        $this->session = \Drupal::request()->getSession();
+        $this->user = \Drupal::currentUser();
+        $this->conn = Database::getConnection();
+    }
+
+    public function getForm()
+    {
+        $registrationForm = $this->formBuilder->getForm('Drupal\seminar\Form\RegisterSeminarForm');
+        return [
+            '#theme' => 'formregistration',
+            '#form' => $registrationForm,
+        ];
+    }
+
+    public function confirm()
+    {
+        $data = $this->request->request->all();
+        $seminar = array(
+            'uid' => $this->user->id(),
+            'first_name' => !empty($data['first_name']) ?  $data['first_name'] : '',
+            'last_name' => !empty($data['last_name']) ?  $data['last_name'] : '',
+            'sex' => !empty($data['sex']) ?  $data['sex'] : '',
+            'phone' => !empty($data['phone']) ?  $data['phone'] : '',
+            'email' => !empty($data['email']) ?  $data['email'] : '',
+            'company_name' => !empty($data['company_name']) ?  $data['company_name'] : '',
+        );
+        if (!$this->saveData($seminar, 'seminar_registration')) {
+            $message = t('Confirm Succerss');
+        } else {
+            $message = t('Confirm fail');
+        }
+        return [
+            '#type' => 'markup',
+            '#markup' => $message,
+        ];
+    }
+
+    public function test()
+    {
+        $taxonomyGender = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('t_gender');
+        print_r($taxonomyGender);
+        return [
+            '#type' => 'markup',
+            '#markup' => t('This aaa'),
+        ];
+    }
+
+    protected function saveData(array $args, $table)
+    {
+        $this->conn->insert($table)->fields($args)->execute();
+    }
+}
---
+++ data/source/drupal/modules/custom/seminar/src/Form/RegisterSeminarForm.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\seminar\Form\RegisterSeminarForm.
+ */
+namespace Drupal\seminar\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+class RegisterSeminarForm extends FormBase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFormId()
+    {
+        return 'seminar_registration';
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        $dataSession = \Drupal::request()->getSession()->get('SEMINAR_REGISTRATION_DATA');
+        if (empty($dataSession)) {
+            $dataSession = \Drupal::request()->getSession()->get('USER_DATA');
+        }
+        $nodeId = \Drupal::request()->getSession()->get('NODE_ID');
+        $form['last_name'] = array(
+            '#type' => 'textfield',
+            '#title' => t('Last Name'),
+            '#size' => 30,
+            '#default_value' => !empty($dataSession['last_name']) ? t($dataSession['last_name']) : '',
+            '#required' => false,
+        );
+        $form['first_name'] = array(
+            '#type' => 'textfield',
+            '#title' => t('First Name'),
+            '#size' => 30,
+            '#default_value' => !empty($dataSession['first_name']) ? t($dataSession['first_name']) : '',
+            '#required' => false,
+        );
+        $taxonomyGender = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('t_gender');
+        $optionGender = [];
+        foreach ($taxonomyGender as $item) {
+            $optionGender[$item->tid] = $this->t($item->name);
+        }
+        $form['sex'] = array(
+            '#type' => 'textfield',
+            '#title' => t('Gender'),
+            '#default_value' => !empty($dataSession['sex']) ? t($dataSession['sex']) : null,
+            '#required' => false,
+        );
+        $form['company_name'] = array(
+            '#type' => 'textfield',
+            '#title' => t('Company Name'),
+            '#size' => 80,
+            '#default_value' => !empty($dataSession['company_name']) ? t($dataSession['company_name']) : '',
+            '#required' => false,
+        );
+        $form['phone'] = array(
+            '#type' => 'textfield',
+            '#title' => t('(Phone Number)'),
+            '#attributes' => array(
+              'oninput' => 'this.value = this.value.replace(/[^0-9.]/g, \'\').replace(/(\..*)\./g, \'$1\');'),
+            '#size' => 11,
+            '#maxlength' => 11,
+            '#pattern' => '[0-9]{9,11}',
+            '#default_value' => !empty($dataSession['phone']) ? t($dataSession['phone']) : '',
+            '#required' => false,
+        );
+        $form['email'] = array(
+            '#type' => 'textfield',
+            '#title' => $this->t('(Email Address)'),
+            '#default_value' => !empty($dataSession['email']) ? t($dataSession['email']) : '',
+            '#attributes' => !empty($dataSession['email']) ? array('readonly' => 'readonly') : array(),
+            '#required' => false,
+        );
+        $form['node_id'] = array(
+            '#type' => 'hidden',
+            '#title' => t(''),
+            '#default_value' => !empty($nodeId) ? $nodeId : '',
+            '#required' => false,
+        );
+        $form['#theme'] = 'formregistration';
+        return $form;
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        // get uid
+        $uid = \Drupal::currentUser()->id();
+        $session = \Drupal::request()->getSession();
+        $formData = [
+            'uid' => $uid,
+            'first_name' => $form_state->getValue('first_name'),
+            'last_name' => $form_state->getValue('last_name'),
+            'email' => $form_state->getValue('email'),
+            'phone' => $form_state->getValue('phone'),
+        ];
+        $session->set('SEMINAR_REGISTRATION_DATA', $formData);
+        $url = Url::fromRoute('seminar.confirm');
+        $form_state->setRedirectUrl($url);
+    }
+
+}
